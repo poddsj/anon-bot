d55f22bfec49b97504890391420fc3128f586122
poddsj <misterhruger@gmail.com>
2026-09-12 18:02:13 +0300

    some fix

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split(",") if x.strip()]
 REQUIRED_CHANNEL = os.environ.get("REQUIRED_CHANNEL", "")
 REQUIRED_CHANNEL_URL = os.environ.get("REQUIRED_CHANNEL_URL", "")
 PROXY = os.environ.get("PROXY") or None
-SUPPORT_CHANNEL_ID = os.environ.get("SUPPORT_CHANNEL_ID")
-SUPPORT_CHANNEL_ID = int(SUPPORT_CHANNEL_ID) if SUPPORT_CHANNEL_ID else None
 
 COOLDOWN = 2 * 60
 DB_PATH = "data.db"
